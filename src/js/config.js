7f020d60a0bccb330c1e2a9e6604cf0ea3a1cb4e
dime-git <dimemilkov1@gmail.com>
2023-02-01 14:07:09 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = '2a23345f-6626-4ce5-8bc8-52285efda089';
+export const MODAL_CLOSE_SEC = 2.5;
